b4eaa02d4061dac9da8848f71efef165d5975a69
MD. SAIF ISLAM <muhammadsaif7717@gmail.com>
2024-08-16 12:23:42 +0600

    Fixed some bugs

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosPublic = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL || 'http://localhost:5000/products',
+    baseURL: import.meta.env.VITE_BASE_URL,
 })
 
 const useAxiosPublic = () => {
